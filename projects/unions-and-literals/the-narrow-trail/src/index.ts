f494c277d14013a4292dc8c4096bb3c40a10850a
dku7 <dku7@proton.me>
2025-01-06 17:05:15 +0000

    create runCommands() function

--- projects/unions-and-literals/the-narrow-trail/src/index.ts
+++ projects/unions-and-literals/the-narrow-trail/src/index.ts
@@ -1,3 +1,76 @@
+const roll = () => Math.floor(Math.random() * 6) + 1;
+
+function getCommand(randomNumber: number) {
+	let command: "food" | "water" | "resupply";
+	let resupplyAmount = 0;
+
+	switch (randomNumber) {
+		case 1:
+			command = "food";
+			break;
+
+		case 2:
+			command = "water";
+			break;
+
+		default:
+			command = "resupply";
+			resupplyAmount = randomNumber;
+			break;
+	}
+
+	return { command, resupplyAmount };
+}
+
 export function runCommands() {
-	// Declare your variables and runtime logic here! ✨
+	let availableResource: "food" | "water" | undefined;
+	let day = 1;
+	let resources = {
+		food: 5,
+		water: 5,
+	};
+
+	while (day <= 7) {
+		const randomNumber = roll();
+		const { command, resupplyAmount } = getCommand(randomNumber);
+
+		if (command === "resupply") {
+			switch (availableResource) {
+				case "food":
+					resources.food += resupplyAmount;
+					availableResource = undefined;
+					break;
+
+				case "water":
+					resources.water += resupplyAmount;
+					availableResource = undefined;
+					break;
+
+				default:
+					availableResource = randomNumber % 2 === 0 ? "food" : "water";
+					break;
+			}
+		}
+
+		switch (command) {
+			case "food":
+				availableResource = "food";
+				break;
+
+			case "water":
+				availableResource = "water";
+				break;
+		}
+
+		resources.food -= 1;
+		resources.water -= 1;
+
+		if (resources.food === 0 || resources.water === 0) {
+			return false;
+		}
+
+		day += 1;
+	}
+
+	return true;
 }
