ae78dda12e6dfe4c481ed034de20d12f140bfe13
dku7 <dku7@proton.me>
2025-01-08 15:52:22 +0000

    implement createCodeCracker function

--- projects/functions/secret-secrets/03-the-golden-code/index.ts
+++ projects/functions/secret-secrets/03-the-golden-code/index.ts
@@ -1,2 +1,22 @@
-// Write your createCodeCracker function here! ✨
-// You'll need to export it so the tests can run it.
+type MakeGuessType = (text: string, attempt: number) => string;
+type ValidateGuessType = (guess: string) => boolean;
+
+type SettingsType = {
+	attempts: number;
+	makeGuess: MakeGuessType;
+	validateGuess: ValidateGuessType;
+};
+
+export function createCodeCracker({
+	attempts,
+	makeGuess,
+	validateGuess,
+}: SettingsType) {
+	return (text: string) => {
+		for (let i = 0; i < attempts; i++) {
+			const guessed = makeGuess(text, i);
+
+			if (validateGuess(guessed)) return guessed;
+		}
+	};
+}
