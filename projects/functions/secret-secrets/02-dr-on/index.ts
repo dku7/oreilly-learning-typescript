2c4ca7cd9c68f176d2041cccf0d973fd3633e63c
dku7 <dku7@proton.me>
2025-01-08 15:23:41 +0000

    create createAdvancedCipher function

--- projects/functions/secret-secrets/02-dr-on/index.ts
+++ projects/functions/secret-secrets/02-dr-on/index.ts
@@ -1,2 +1,21 @@
-// Write your createAdvancedCipher function here! ✨
-// You'll need to export it so the tests can run it.
+type CipherCallback = (character: string) => string;
+
+export function createAdvancedCipher(
+	onVowel: CipherCallback,
+	onConsonant: CipherCallback,
+	onPunctuation: CipherCallback
+) {
+	return (text: string) => {
+		let str = "";
+		const vowelRegEx = /[aeiou]/i;
+		const consonantRegEx = /[b-df-hj-np-tv-z]/i;
+
+		for (const character of text) {
+			if (vowelRegEx.test(character)) str += onVowel(character);
+			else if (consonantRegEx.test(character)) str += onConsonant(character);
+			else str += onPunctuation(character);
+		}
+
+		return str;
+	};
+}
