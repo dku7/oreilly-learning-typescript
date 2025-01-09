2c801931e71096403b7b785c36b7f68edc352aa2
dku7 <dku7@proton.me>
2025-01-09 10:02:10 +0000

    implement deepDifferences function

--- projects/arrays/analyzing-dna/04-deep-differences/index.ts
+++ projects/arrays/analyzing-dna/04-deep-differences/index.ts
@@ -1,2 +1,20 @@
-// Write your deepDifferences function here! ✨
-// You'll need to export it so the tests can run it.
+export function deepDifferences(a: string[][], b: string[][]) {
+	const result: ((string | undefined)[] | undefined)[] = [];
+
+	for (let i = 0; i < a.length; i++) {
+		if (a[i].length !== b[i].length) result.push(undefined);
+		else {
+			const arr: (string | undefined)[] = [];
+
+			for (let j = 0; j < a[i].length; j++) {
+				const value = a[i][j] === b[i][j] ? a[i][j] : undefined;
+
+				arr.push(value);
+			}
+
+			result.push(arr);
+		}
+	}
+
+	return result;
+}
