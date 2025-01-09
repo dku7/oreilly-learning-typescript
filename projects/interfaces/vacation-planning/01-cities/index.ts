91fb24292e70b53dcfc4991b435a6912668222c2
dku7 <dku7@proton.me>
2025-01-09 11:50:28 +0000

    implement describeCity function

--- projects/interfaces/vacation-planning/01-cities/index.ts
+++ projects/interfaces/vacation-planning/01-cities/index.ts
@@ -1,2 +1,31 @@
-// Write your describeCity function here! ✨
-// You'll need to export it so the tests can run it.
+type Coordinate = [number, number, number];
+
+interface Coordinates {
+	north: Coordinate;
+	west: Coordinate;
+}
+
+interface City {
+	coordinates: Coordinates;
+	name: string;
+	catchphrase?: string;
+}
+
+const padNum = (num: number) => String(num).padStart(2, "0");
+
+const formatCoord = ([x, y, z]: Coordinate) =>
+	`${padNum(x)}°${padNum(y)}'${padNum(z)}"`;
+
+const formatCoords = (coords: Coordinates) =>
+	`\n* Located at ${formatCoord(coords.north)}N ${formatCoord(coords.west)}W`;
+
+const formatName = (name: string) => `${name}, New York`;
+
+const formatCatchphrase = (catchphrase: string | undefined) =>
+	catchphrase ? `\n* "${catchphrase}"` : "";
+
+export function describeCity({ coordinates, name, catchphrase }: City) {
+	return `${formatName(name)}${formatCatchphrase(catchphrase)}${formatCoords(
+		coordinates
+	)}`;
+}
