b3d3bd59b3b451fb2b8018d5e3d59739dcc818d7
dku7 <dku7@proton.me>
2025-01-09 09:21:03 +0000

    refactor to use join

--- projects/arrays/analyzing-dna/01-shallow-equality/index.ts
+++ projects/arrays/analyzing-dna/01-shallow-equality/index.ts
@@ -1,9 +1,3 @@
 export function shallowEquality(a: string[], b: string[]) {
-	if (a.length !== b.length) return false;
-
-	for (let i = 0; i < a.length; i++) {
-		if (a[i] !== b[i]) return false;
-	}
-
-	return true;
+	return a.join("") === b.join("");
 }
