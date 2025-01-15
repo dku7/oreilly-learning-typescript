030568701dcbbc59c14de9b9234c98795e6f2c28
dku7 <dku7@proton.me>
2025-01-15 08:56:12 +0000

    implement Horror, Demon, and Sorcerer classes

--- projects/classes/the-shape-of-types/src/index.ts
+++ projects/classes/the-shape-of-types/src/index.ts
@@ -1,2 +1,61 @@
-// Write your classes here! ✨
-// You'll need to export them so the tests can run them.
+interface Consumed {
+	evil: boolean;
+	name: string;
+	power: number;
+}
+
+export abstract class Horror {
+	abstract readonly name: string;
+	#consumed: Consumed[] = [];
+
+	doBattle(opponent: Horror) {
+		if (this.getPower() >= opponent.getPower()) {
+			this.#consumed.push({
+				evil: opponent.isEvil(),
+				name: opponent.name,
+				power: opponent.getPower(),
+			});
+		}
+	}
+
+	getPower() {
+		return this.#consumed.reduce(
+			(previous, consumed) => previous + this.getPowerFrom(consumed),
+			this.#consumed.length
+		);
+	}
+
+	protected abstract getPowerFrom(consumed: Consumed): number;
+	protected abstract isEvil(): boolean;
+}
+
+export class Demon extends Horror {
+	name = "Demon";
+
+	getPowerFrom(consumed: Consumed) {
+		return consumed.evil ? consumed.power / 2 : consumed.power * 2;
+	}
+
+	isEvil() {
+		return true;
+	}
+}
+
+export class Sorcerer extends Horror {
+	readonly name: string;
+	#evil: boolean;
+
+	constructor(name: string, evil: boolean) {
+		super();
+		this.name = name;
+		this.#evil = evil;
+	}
+
+	getPowerFrom(consumed: Consumed) {
+		return consumed.evil === this.#evil ? consumed.power * 2 : consumed.power;
+	}
+
+	isEvil() {
+		return this.#evil;
+	}
+}
