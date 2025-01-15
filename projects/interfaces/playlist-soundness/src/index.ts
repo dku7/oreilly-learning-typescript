210b3e9c9c643557cc39758808962b121e0b2f29
dku7 <dku7@proton.me>
2025-01-15 15:27:44 +0000

    create unrollPlaylist function and related interfaces

--- projects/interfaces/playlist-soundness/src/index.ts
+++ projects/interfaces/playlist-soundness/src/index.ts
@@ -1,2 +1,63 @@
-// Write your unrollPlaylist function and types here! ✨
-// You'll need to export the function so the tests can run it.
+interface Artists {
+	[i: string]: string[];
+}
+
+interface UnrolledPlaylist {
+	artists: Artists;
+	songs: string[];
+	time: number;
+}
+
+interface Song {
+	artist: string | string[];
+	length: number;
+	name: string;
+	type: "song";
+}
+
+interface Album {
+	songs: Song[];
+	type: "album";
+}
+
+interface Playlist {
+	resolve(): Song[];
+	type: "playlist";
+}
+
+type PlaylistItem = Song | Album | Playlist;
+
+export function unrollPlaylist(items: PlaylistItem[]): UnrolledPlaylist {
+	const artists: Artists = {};
+	const songs: string[] = [];
+	let time = 0;
+
+	const addSong = (song: Song) => {
+		const songArtists =
+			typeof song.artist === "string" ? [song.artist] : song.artist;
+
+		for (const artist of songArtists) {
+			artists[artist] ??= [];
+			artists[artist].push(song.name);
+		}
+
+		time += song.length;
+		songs.push(song.name);
+	};
+
+	for (const item of items) {
+		switch (item.type) {
+			case "song":
+				addSong(item);
+				break;
+			case "album":
+				item.songs.forEach(addSong);
+				break;
+			case "playlist":
+				item.resolve().forEach(addSong);
+				break;
+		}
+	}
+
+	return { artists, songs, time };
+}
