8b63929db2ca6aa8b1e93f3d71be7c0bd76daebf
dku7 <dku7@proton.me>
2025-01-16 17:03:35 +0000

    add type declarations

--- projects/declaration-files/type-illusions/src/show.d.ts
+++ projects/declaration-files/type-illusions/src/show.d.ts
@@ -1,1 +1,29 @@
-// Declare your types here! ✨
+export interface Trick {
+	gimmick: string;
+}
+
+export interface Illusion {
+	introduction(): string;
+	flair(): string;
+	payoff(): string;
+}
+
+export interface VolunteerIllusion extends Illusion {
+	duration: number;
+	title: string;
+}
+
+export interface AudienceMember {
+	name: string;
+}
+
+export interface Act {
+	performer: string;
+	name: string;
+	sections: (Trick | Illusion)[];
+}
+
+export function getAudienceMemberFor(section: {
+	duration: number;
+	title: string;
+}): Promise<AudienceMember>;
