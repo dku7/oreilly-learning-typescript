2c3581de1eededcbe437ef456eb10de422adda1f
dku7 <dku7@proton.me>
2025-01-16 10:23:57 +0000

    implement zip function

--- projects/generics/hidash/02-zip/index.ts
+++ projects/generics/hidash/02-zip/index.ts
@@ -1,2 +1,11 @@
-// Write your zip function here! ✨
-// You'll need to export it so the tests can run it.
+export function zip<T, U>(a: T[], b: U[]) {
+	const result: (T | U)[] = [];
+	const length = Math.max(a.length, b.length);
+
+	for (let i = 0; i < length; i++) {
+		if (i < a.length) result.push(a[i]);
+		if (i < b.length) result.push(b[i]);
+	}
+
+	return result;
+}
