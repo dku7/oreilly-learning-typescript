c99224b1f3182a67ba31fe0f93cce6a4579f7428
dku7 <dku7@proton.me>
2025-01-16 14:23:27 +0000

    implement pickMany function

--- projects/generics/hidash/04-pickmany/index.ts
+++ projects/generics/hidash/04-pickmany/index.ts
@@ -1,2 +1,6 @@
-// Write your pickMany function here! ✨
-// You'll need to export it so the tests can run it.
+export function pickMany<T, Key extends keyof T>(
+	container: T,
+	keys: Key[]
+): T[Key][] {
+	return keys.map((key) => container[key]);
+}
